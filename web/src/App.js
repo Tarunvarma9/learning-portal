3e6b1528aa647f84f072412549843a9a31f5d8c1
Tarunvarma9 <68417534+Tarunvarma9@users.noreply.github.com>
2021-12-10 17:00:07 +0530

    ui changes

--- web/src/App.js
+++ web/src/App.js
@@ -6,11 +6,12 @@ import Profile from './components/profile/Profile';
 import LoginForm from './components/login/LoginForm';
 import history from './components/history';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-
+import Dashboard from './components/dashboard/Dashboard';
 function App() {
   return (
     <Router>
     <Routes>
+      <Route exact path='/' element={<Dashboard/>}/>
       <Route exact path='/login' element={<LoginForm/>}/>
       <Route exact path='/profile' element={<Profile/>}/>
     </Routes>
--- web/src/components/profile/Profile.jsx
+++ web/src/components/profile/Profile.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-function profile() {
+function Profile() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/users_details/")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
+  console.log(users)
   return (
-    <div>
-      <h1>
-        Guzman
-      </h1>
-      
+    <>
+    <div style={{textAlign:'right', fontSize:'1rem', margin:'30px 3%'}}>
+      <Link to='/' style={{textDecoration:'none', color:'#000',padding:'5px 3%', boxShadow:"0 3px 10px rgb(0 0 0 / 0.2)"}}>
+      Get back to Dashboard
+      </Link>
     </div>
-  )
+    
+    <div style={{padding:'50px 10%'}}>
+        <h1>USE_R</h1>
+      {users.map((user) =>
+       (
+        <div key={user.id} style={{boxShadow:'0 3px 10px rgb(0 0 0 / 0.2)', padding:"20px 5%"}}>
+           <h6>
+               {user.name}
+           </h6>
+           <p>
+               {user.id}
+           </p>
+           <h2>
+               {user.user_name}
+           </h2>
+           <h6>
+             {user.email}  
+           </h6>
+        </div>
+      ))}
+    </div>
+  </>
+  );
 }
 
-export default profile
+export default Profile;
